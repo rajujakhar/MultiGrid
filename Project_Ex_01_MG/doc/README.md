163701f68d9545b4abf575d6ed31e52602270df7
r.rajujakhar1993@gmail.com <pi02dawu@stud.informatik.uni-erlangen.de>
2016-05-09 17:29:10 +0200

    repo removed

--- Project_Ex_01_MG/doc/README.md
+++
@@ -1,2 +0,0 @@
-# MultiGrid
-This repo contains C++ programs written as part of first assignment of SiwiR2.
--- Project_Ex_01_MG/include/Precision.h
+++
@@ -1,7 +0,0 @@
-#ifndef PRECISION_H
-#define PRECISION_H
-
-// Lets rename double as real in the assignment
-typedef double real;
-
-#endif
